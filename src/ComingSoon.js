24b50220b402993a12dcb2988761c5c3eed6c44a
DestinysProblemChild <destinysproblmchild@gmail.com>
2025-03-31 01:12:38 -0700

    Update ComingSoon.js

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -4,7 +4,7 @@ const ComingSoon = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
       <h1>Coming Soon</h1>
-      <p>We're working hard to bring you the best experience. Stay tuned!</p>
+      <p>I'm working hard to bring you the best experience. Stay tuned!</p>
     </div>
   );
 };
