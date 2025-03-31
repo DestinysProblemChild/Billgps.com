2b6b39fae3cdf48556e39e92a56624ce88c74830
Destinys Problem Child <destinysproblmchild@gmail.com>
2025-03-31 02:00:38 -0700

    Updated text on Coming Soon page

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -4,7 +4,7 @@ const ComingSoon = () => {
   return (
     <div style={{ textAlign: 'center', marginTop: '100px' }}>
       <h1>Coming Soon</h1>
-      <p>I'm working hard to bring you the best experience. Stay tuned!</p>
+      <p>We're working hard to bring you the best experience. Stay tuned!</p>
       {/* Image from the public folder */}
       <img 
         src="/199cd3b60d64297ad6608dbf7870fe6b.jpg"  // Path to the image in public folder
