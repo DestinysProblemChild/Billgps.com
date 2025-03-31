297ba8012edb31c82e4bc8b5ebd7394c9e1e305d
Destinys Problem Child <destinysproblmchild@gmail.com>
2025-03-31 00:53:23 -0700

    Initial commit - Add Coming Soon page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ComingSoon from './ComingSoon';  // Import the ComingSoon component
+
+function App() {
+  return (
+    <div className="App">
+      <ComingSoon />  {/* Use the ComingSoon component */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ComingSoon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ComingSoon = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Coming Soon</h1>
+      <p>We're working hard to bring you the best experience. Stay tuned!</p>
+    </div>
+  );
+};
+
+export default ComingSoon;
