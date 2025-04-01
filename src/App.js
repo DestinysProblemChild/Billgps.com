d23b9ff927f839881e5ba227d90c9c05e1ee62d9
Destinys Problem Child <destinysproblmchild@gmail.com>
2025-03-31 22:55:08 -0700

    Update src/App.js and ComingSoon.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import BillsPage from './BillsPage';  // Correct import for BillsPage
+import BillsPage from './Billspage'; // Correct import
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<BillsPage />} />  {/* Correct way to render BillsPage */}
+        <Route path="/" element={<BillsPage />} /> {/* Correct way to render BillsPage */}
       </Routes>
     </Router>
   );
